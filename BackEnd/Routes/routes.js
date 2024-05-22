eda176241ac16140f613cd1cbfc734140641ea9b
StockTutorMarketing <stocktutor.marketing@gmail.com>
2024-05-23 00:14:59 +0530

    Delete BackEnd directory

--- BackEnd/Routes/routes.js
+++
@@ -1,7 +0,0 @@
-const { newOrderId, checkStatus } = require("../Controller/cashfree")
-const express = require("express")
-const router = express();
-router.post("/payment", newOrderId)
-router.get("/status/:orderid",checkStatus)
-
-module.exports = router
